3fc1508d202c012d8f7588f5ffbfdb2a79bd8d2c
kangxw <254882274@qq.com>
2018-08-20 23:19:25 +0800

    为对方如果

---
+++ src/fgd/GGJ.java
@@ -0,0 +1,24 @@
+package fgd;
+
+public class GGJ {
+
+	public static void main(String[] args) {
+		/*sdfjsdfh
+		 * sdfksdfjl
+		 * dsfsdf
+		 * dsfsdf
+		 * dsfsdfhggfggf
+		 * dsfsd
+		 * fsdf
+		 * sdfsd
+		 * f
+		 * gfghfgh
+		 * 
+		 * sdfsdfsdfs
+		 * 
+		 * fdgdfg
+		 * 
+		 */
+		
+	}
+}
---
+++ src/fgd/KKK.java
@@ -0,0 +1,18 @@
+package fgd;
+
+public class KKK {
+
+	public static void main(String[] args) {
+		//sdfsdfdfdfkjisfkfdsf test moosdfk
+		//dsfiksjdfisdfkjkjk
+// Upstream, based on branch 'test' of https://github.com/kangxingwei/jdktest
+		//fdgfdgdfg
+		//fdgfg
+//
+		//gdfgdfg/fdgdfgdf/dfgdf
+		
+		//fdgdfgdfdfgfdg/
+//   e846c3d fghjk
+		//fdgdfgfdg
+	}
+}
